539586643ae3cf50d8bf376b2cb20090a54b5995
William Ryuputra <williamryuputra@yahoo.co.id>
2021-09-22 22:02:59 -0400

    Modified warning text

--- src/components/warning.js
+++ src/components/warning.js
@@ -18,7 +18,7 @@ function Warning() {
             <p>The device you are using has been detected <br></br>
             Location : 83 Carleton St Hamden CT 06517</p>
 
-            <button onClick={notify}>WARNING</button> 
+            <button onClick={notify}>Click To Exit</button> 
         
             <div className = 'map'>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d529.5130454369341!2d-72.91456733208376!3d41.344134334031736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e7da1d8e23bd4b%3A0x992795689eda0b9a!2s83%20Carleton%20St%2C%20Hamden%2C%20CT%2006517!5e0!3m2!1sen!2sus!4v1632359294389!5m2!1sen!2sus" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
